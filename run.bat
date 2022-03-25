f7d64e170a0c1212ee725b98d354dc089d3019cf
SuYenTing <sn86056@gmail.com>
2022-03-25 21:47:31 +0800

    change the prompt description from Chinese to English.

--- run.bat
+++ run.bat
@@ -1,44 +1,46 @@
+chcp 65001
+
 @echo off
 
-echo 檢查目前資料夾是否有安裝虛擬環境 & echo.
+echo Check whether the current folder has a virtual environment installed. & echo.
 
 if exist .\env\d4sg-cipas-env\ (
 
-  echo 目前已有安裝虛擬環境 & echo.
+  echo A virtual environment is currently installed. & echo.
 
 ) else (
 
-  echo 未發現虛擬環境 開始建立Python虛擬環境... & echo.
+  echo No virtual environment found. Start to create a Python virtual environment... & echo.
 
   python -m venv ./env/d4sg-cipas-env
 
-  echo Python虛擬環境已建立完成! & echo.
+  echo Python virtual environment has been established. & echo.
 
-  echo 開始安裝相關套件 請稍等... & echo.
+  echo Start installing related packages, please wait... & echo.
 
   call .\env\d4sg-cipas-env\Scripts\activate.bat & python -m pip install -r .\env\requirements.txt
 
-  echo 套件已安裝完畢! & echo.
+  echo Python packages are all installed! & echo.
 
-  echo Python虛擬環境建置已完成 & echo.
+  echo Python virtual environment setup has been completed. & echo.
 )
 
 
-choice /M "請問是否要進行CKIP斷詞與實體辨識? [Yes/No]"
+choice /M "Do you want to run CKIP word segmentation and name entity recognition? [Yes/No]"
 
 if %errorlevel%==1 (
 
-  echo 開始進行CKIP斷詞與實體辨識 & echo.
+  echo Start CKIP word segmentation and entity recognition. & echo.
   call .\env\d4sg-cipas-env\Scripts\activate.bat & python ./model/docTokenize.py
 )
 
 if %errorlevel%==2 (
 
-  echo 跳過CKIP斷詞與實體辨識 & echo.
+  echo Skip CKIP segmentation and entity recognition. & echo.
 
 )
 
-echo 開始建立WEB環境 & echo.
+echo Start building a WEB environment... & echo.
 call .\env\d4sg-cipas-env\Scripts\activate.bat & python ./web/index.py
 
 pause
