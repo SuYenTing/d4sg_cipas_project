90c6d7052ca4569e82e3c38a3db3f162fb82710d
SuYenTing <sn86056@gmail.com>
2022-03-26 12:24:26 +0800

    open host and close debug mode

--- web/index.py
+++ web/index.py
@@ -97,5 +97,5 @@ def display_page(pathname):
 
 if __name__ == "__main__":
 
-    app.run_server(debug=True, port=8050)
-    # app.run_server(host='0.0.0.0', debug=False, port=8050)  # 開啟對外連線
+    # app.run_server(debug=True, port=8050)
+    app.run_server(host='0.0.0.0', debug=False, port=8050)  # 開啟對外連線
